60a3bca09ace71b9cd1a6175aeab1e2163f5bdc3
zekeinsaneprolololol <republikekurtishi@gmail.com>
2025-04-22 22:48:14 +0200

    Update bgsi.lua

--- bgsi.lua
+++ bgsi.lua
@@ -1,138 +1,151 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
 
-local Window = Rayfield:CreateWindow({
-	Name = "lost azure",
-	Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-	LoadingTitle = "lost azure",
-	LoadingSubtitle = "by benihime",
-	Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
- 
-	DisableRayfieldPrompts = false,
-	DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
- 
-	ConfigurationSaving = {
-	   Enabled = true,
-	   FolderName = nil, -- Create a custom folder for your hub/game
-	   FileName = "lost azure was here"
+local Window = Luna:CreateWindow({
+	Name = "Sentinel Hub", -- This Is Title Of Your Window
+	Subtitle = nil, -- A Gray Subtitle next To the main title.
+	LogoID = "82795327169782", -- The Asset ID of your logo. Set to nil if you do not have a logo for Luna to use.
+	LoadingEnabled = true, -- Whether to enable the loading animation. Set to false if you do not want the loading screen or have your own custom one.
+	LoadingTitle = "Sentinel Interface Suite", -- Header for loading screen
+	LoadingSubtitle = "by Nebula Softworks", -- Subtitle for loading screen
+
+	ConfigSettings = {
+		RootFolder = nil, -- The Root Folder Is Only If You Have A Hub With Multiple Game Scripts and u may remove it. DO NOT ADD A SLASH
+		ConfigFolder = "Big Hub" -- The Name Of The Folder Where Luna Will Store Configs For This Script. DO NOT ADD A SLASH
 	},
- 
-	Discord = {
-	   Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-	   Invite = "FrNHEhJYHH", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-	   RememberJoins = false -- Set this to false to make them join the discord every time they load it up
-	},
- 
-	KeySystem = false, -- Set this to true to use our key system
+
+	KeySystem = false, -- As Of Beta 6, Luna Has officially Implemented A Key System!
 	KeySettings = {
-	   Title = "Key",
-	   Subtitle = "Key System",
-	   Note = "Join the Discord", -- Use this to tell the user how to get a key
-	   FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-	   SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-	   GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-	   Key = {"lost azure real"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+		Title = "Sentinel Hub",
+		Subtitle = "Key System",
+		Note = "Best Key System Ever! Also, Please Use A HWID Keysystem like Pelican, Luarmor etc. that provide key strings based on your HWID since putting a simple string is very easy to bypass",
+		SaveInRoot = false, -- Enabling will save the key in your RootFolder (YOU MUST HAVE ONE BEFORE ENABLING THIS OPTION)
+		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+		Key = {"WWW"}, -- List of keys that will be accepted by the system, please use a system like Pelican or Luarmor that provide key strings based on your HWID since putting a simple string is very easy to bypass
+		SecondAction = {
+			Enabled = true, -- Set to false if you do not want a second action,
+			Type = "Link", -- Link / Discord.
+			Parameter = "" -- If Type is Discord, then put your invite link (DO NOT PUT DISCORD.GG/). Else, put the full link of your key system here.
+		}
 	}
- })
-
- local Tab1 = Window:CreateTab("Auto Farm", 4483362458) -- Title, Image
-
- local Section = Tab1:CreateSection("join my discord pls lel")
-
-Rayfield:Notify({
-   Title = "Loaded!",
-   Content = "Have Fun",
-   Duration = 6.5,
-   Image = 4483362458,
 })
 
-local Toggle = Tab1:CreateToggle({
-	Name = "Auto Bubble",
-	CurrentValue = false,
-	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(value)
-    end
+Window:CreateHomeTab({
+	SupportedExecutors = {}, -- A Table Of Executors Your Script Supports. Add strings of the executor names for each executor.
+	DiscordInvite = "1234", -- The Discord Invite Link. Do Not Include discord.gg/ | Only Include the code.
+	Icon = 1, -- By Default, The Icon Is The Home Icon. If You would like to change it to dashboard, replace the interger with 2
 })
 
- 
- local Toggle = Tab1:CreateToggle({
-	Name = "Auto Collect Coins",
-	CurrentValue = false,
-	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(value)
-	end,
- })
 
- local Toggle = Tab1:CreateToggle({
+local Auto Farm = Window:CreateTab({
+	Name = "Auto Farm",
+	Icon = "view_in_ar",
+	ImageSource = "Material",
+	ShowTitle = true -- This will determine whether the big header text in the tab will show
+})
+
+local Teleports = Window:CreateTab({
+	Name = "Teleports",
+	Icon = "view_in_ar",
+	ImageSource = "Material",
+	ShowTitle = true -- This will determine whether the big header text in the tab will show
+})
+
+local Misc = Window:CreateTab({
+	Name = "Misc",
+	Icon = "view_in_ar",
+	ImageSource = "Material",
+	ShowTitle = true -- This will determine whether the big header text in the tab will show
+})
+
+Luna:Notification({ 
+	Title = "Sentinel Hub",
+	Icon = "notifications_active",
+	ImageSource = "Material",
+	Content = "Loaded!"
+})
+
+local Label = Auto Farm:CreateLabel({
+	Text = "This released Early Expect bugs. (send bugs in dc)",
+	Style = 3 -- Luna Labels Have 3 Styles : A Basic Label, A Green Information Label and A Red Warning Label. Look At The Following Image For More Details
+})
+
+local Toggle = Auto Farm:CreateToggle({
+	Name = "Auto Pick up Coins",
+	Description = nil,
+	CurrentValue = false,
+    	Callback = function(Value)
+       	 -- The function that takes place when the toggle is switched
+       	 -- The variable (Value) is a boolean on whether the toggle is true or false
+    	end
+}, "Toggle") -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+
+local Toggle = Auto Farm:CreateToggle({
+	Name = "Auto Blow Bubbles",
+	Description = nil,
+	CurrentValue = false,
+    	Callback = function(Value)
+       	 -- The function that takes place when the toggle is switched
+       	 -- The variable (Value) is a boolean on whether the toggle is true or false
+    	end
+}, "Toggle") -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+
+local Toggle = Auto Farm:CreateToggle({
 	Name = "Auto Sell",
+	Description = nil,
 	CurrentValue = false,
-	Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(value)
-	end,
- })
+    	Callback = function(Value)
+       	 -- The function that takes place when the toggle is switched
+       	 -- The variable (Value) is a boolean on whether the toggle is true or false
+    	end
+}, "Toggle") -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
 
- local Dropdown = Tab1:CreateDropdown({
-	Name = "Auto Mastery",
-	Options = {"None","Shop","Pet",},
-	CurrentOption = {"None"},
-	MultipleOptions = false,
-	Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(Options)
-	-- The function that takes place when the selected option is changed
-	-- The variable (Options) is a table of strings for the current selected options
-	end,
- })
-
- local Tab2 = Window:CreateTab("Teleport", 4483362458) -- Title, Image
-
-
- local Button = Tab2:CreateButton({
+local Button = Teleports:CreateButton({
 	Name = "Floating Island",
-	Callback = function()
-	-- The function that takes place when the button is pressed
-	end,
- })
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         -- The function that takes place when the button is pressed
+    	end
+})
 
- local Button = Tab2:CreateButton({
+local Button = Teleports:CreateButton({
 	Name = "Outer Space",
-	Callback = function()
-	-- The function that takes place when the button is pressed
-	end,
- })
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         -- The function that takes place when the button is pressed
+    	end
+})
 
- local Button = Tab2:CreateButton({
+local Button = Teleports:CreateButton({
 	Name = "Twilight",
-	Callback = function()
-	-- The function that takes place when the button is pressed
-	end,
- })
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         -- The function that takes place when the button is pressed
+    	end
+})
 
- local Button = Tab2:CreateButton({
+local Button = Teleports:CreateButton({
 	Name = "The Void",
-	Callback = function()
-	-- The function that takes place when the button is pressed
-	end,
- })
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         -- The function that takes place when the button is pressed
+    	end
+})
 
- local Button = Tab2:CreateButton({
+local Button = Teleports:CreateButton({
 	Name = "Zen",
-	Callback = function()
-	-- The function that takes place when the button is pressed
-	end,
- })
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+         -- The function that takes place when the button is pressed
+    	end
+})
 
-
- local Tab3 = Window:CreateTab("Misc", 4483362458) -- Title, Image
-
- local Slider = Tab3:CreateSlider({
+local Slider = Misc:CreateSlider({
 	Name = "Trampoline Power",
-	Range = {0.5, 10},
-	Increment = 10,
-	Suffix = "Percent",
-	CurrentValue = 1.25,
-	Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-	Callback = function(Value)
-		
-	end,
- })
-
-
+	Range = {0, 10}, -- The Minimum And Maximum Values Respectively
+	Increment = 0.5, -- Basically The Changing Value/Rounding Off
+	CurrentValue = 1.25, -- The Starting Value
+    	Callback = function(Value)
+       	 -- The function that takes place when the slider changes
+       	 -- The variable (Value) is a number which correlates to the value the slider is currently at
+    	end
+}, "Slider") -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
